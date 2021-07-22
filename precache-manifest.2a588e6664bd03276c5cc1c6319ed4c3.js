d9a2493809698f1c2f8f85e5b374d553b2aab703
Сакевич Анастасія <asakevych@ASMAC.local>
2021-07-22 16:32:09 +0200

    Updates

---
+++ precache-manifest.2a588e6664bd03276c5cc1c6319ed4c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e8407903b80bc277bc7b75853b78b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dfb10ccf37a916dee940",
+    "url": "./static/css/main.c22150dd.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb10ccf37a916dee940",
+    "url": "./static/js/main.5bea3a68.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  },
+  {
+    "revision": "5d03c415bf0d2347681049cda442ae6c",
+    "url": "./static/media/kisspng-vomiting-unicorn-rainbow-nausea-disease-unicorn-5ab613ed687ff0.6862365115218820934281.5d03c415.png"
+  }
+]);
